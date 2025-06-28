9f8bee4149afacbdc9e2971aad10fdc54c9be004
reru96 <renatorumieri96@outlook.com>
2025-06-28 17:49:53 +0200

    ItemDictionary e InventorySaveData
    aggiunto un dizionario degli Item dove inserire man mano gli item di gioco, ed un Metodo di salvataggio nell'inventario (da testare)

--- Assets/Scripts/Savings/DataSave.cs
+++ Assets/Scripts/Savings/DataSave.cs
@@ -1,9 +1,12 @@
+using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
   
 [System.Serializable]
 public class DataSave 
 {
  
   public Vector3 playerPosition;
+  public List<InventorySaveData> inventorySaveData;
     
 }
--- Assets/Scripts/Savings/Save.cs
+++ Assets/Scripts/Savings/Save.cs
@@ -1,17 +1,16 @@
 using System.IO;
-using UnityEditor.Overlays;
 using UnityEngine;
 
 public class Save : MonoBehaviour
 {
-
     public string saveLocation;
     private GameObject player;
+    private InventoryController inventoryController;
 
     void Start()
     {
         saveLocation = Path.Combine(Application.persistentDataPath, "saveData.json");
-
+        inventoryController = FindAnyObjectByType<InventoryController>();
         
         player = GameObject.FindWithTag("Family");
 
@@ -27,8 +26,10 @@ public class Save : MonoBehaviour
 
         DataSave saveData = new DataSave()
         {
-            playerPosition = player.transform.position
+            playerPosition = player.transform.position,
+            inventorySaveData = inventoryController.GetInventoryItems()
         };
+
 
         File.WriteAllText(saveLocation, JsonUtility.ToJson(saveData));
     }
@@ -38,7 +39,8 @@ public class Save : MonoBehaviour
         if (File.Exists(saveLocation))
         {
             DataSave saveData = JsonUtility.FromJson<DataSave>(File.ReadAllText(saveLocation));
-            player.transform.position = saveData.playerPosition;
+            GameObject.FindGameObjectWithTag("Family").transform.position = saveData.playerPosition;
+            inventoryController.SetInventoryItem(saveData.inventorySaveData);
         }
         else
         {
