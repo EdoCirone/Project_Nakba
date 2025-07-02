e13b74745474590f3192eb758c05206eef74f2fc
EdoCirone <edoardocirone@gmail.com>
2025-07-02 11:19:48 +0200

    inizio col lvl design

--- Assets/Scripts/Interface/BasicUIManager.cs
+++ Assets/Scripts/Interface/BasicUIManager.cs
@@ -5,12 +5,15 @@ public class BasicUIManager : MonoBehaviour
     public GameObject pauseMenu; // Reference to the pause menu UI
     public void OnMenuPress()
     {
+        if (pauseMenu == null)
+        {
+            Debug.LogError("Pause menu is not assigned in the BasicUIManager.");
+            return;
+        }
+
         pauseMenu.SetActive(true); // Show the pause menu
         Debug.Log("Menu pressed. Pausing game.");
         Time.timeScale = 0f; // Pause game time
     }
-
-
-
 
 }
